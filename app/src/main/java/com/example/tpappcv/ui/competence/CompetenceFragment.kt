bf608f1050e7a9b26320b5b84e098672373ac06d
AmineC95 <amine.chioukh95@gmail.com>
2023-02-05 23:10:36 +0100

    Update Databinding

--- app/src/main/java/com/example/tpappcv/ui/competence/CompetenceFragment.kt
+++ app/src/main/java/com/example/tpappcv/ui/competence/CompetenceFragment.kt
@@ -4,37 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.tpappcv.MyData
 import com.example.tpappcv.databinding.FragmentCompetenceBinding
 
 class CompetenceFragment : Fragment() {
 
-private var _binding: FragmentCompetenceBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentCompetenceBinding? = null
+    private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val competenceViewModel =
-            ViewModelProvider(this).get(CompetenceViewModel::class.java)
-
-    _binding = FragmentCompetenceBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
-    val textView: TextView = binding.textCompetence
-    competenceViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCompetenceBinding.inflate(inflater, container, false)
+        binding.user = MyData("CHIOUKH Amine", "amine.chioukh95@gmail.com")
+        return binding.root
     }
-    return root
-  }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
--- app/src/main/java/com/example/tpappcv/ui/competence/CompetenceViewModel.kt
+++ app/src/main/java/com/example/tpappcv/ui/competence/CompetenceViewModel.kt
@@ -3,11 +3,12 @@ package com.example.tpappcv.ui.competence
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tpappcv.MyData
 
 class CompetenceViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is competence Fragment"
+    private val _data = MutableLiveData<MyData>().apply {
+        value = MyData("CHIOUKH Amine", "amine.chioukh95@gmail.com")
     }
-    val text: LiveData<String> = _text
+    val data: LiveData<MyData> = _data
 }
